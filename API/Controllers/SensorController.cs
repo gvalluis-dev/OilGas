613c5281f4e02cf3621f3e7a86e7fada1294c967
Gabriel Valluis <gvalluis@gmail.com>
2024-10-11 19:07:11 -0300

    adding swashbuckle for documentation

--- API/Controllers/SensorController.cs
+++ API/Controllers/SensorController.cs
@@ -3,6 +3,8 @@ using OilGas.Models;
 using Microsoft.AspNetCore.Mvc;
 using OilGas.DTO;
 using OilGas.Services;
+using Microsoft.EntityFrameworkCore;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace OilGas.Controllers
 {
@@ -16,6 +18,11 @@ namespace OilGas.Controllers
             _sensorDataService = sensorDataService;
         }
 
+        /// <summary>
+        /// Insere o dado de um sensor, de forma unitária
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
         [HttpPost("sensor")]
         public async Task<IActionResult> PostSensorData([FromBody] SensorDataDTO data)
         {
@@ -23,6 +30,11 @@ namespace OilGas.Controllers
             return Ok();
         }
 
+        /// <summary>
+        /// Insere dados em lote usando um arquivo CSV
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns></returns>
         [HttpPost("upload-csv")]
         public async Task<IActionResult> UploadCsv(IFormFile file)
         {
@@ -31,6 +43,60 @@ namespace OilGas.Controllers
 
             await _sensorDataService.AddSensorDataCSV(file);
             return Ok("Dados dos sensores importados com sucesso");
+        }
+
+        /// <summary>
+        /// Calcula a média dos valores dos sensores em um determinado período.
+        /// </summary>
+        /// <param name="period">O período para o cálculo da média. Valores possíveis: 24h, 48h, 1w (1 semana), 1m (1 mês).</param>
+        /// <returns>A média dos valores dos sensores no período especificado.</returns>
+        [HttpGet("average")]
+        [SwaggerOperation(Summary = "Calcula a média dos valores dos sensores em um determinado período.",
+                      Description = "Períodos disponíveis: 24h, 48h, 1w (1 semana), 1m (1 mês).")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult GetSensorAverage([FromQuery] string period)
+        {
+            // Definir o intervalo de tempo com base no parâmetro 'period'
+            DateTime fromDate = DateTime.Now;
+
+            switch (period.ToLower())
+            {
+                case "24h":
+                    fromDate = DateTime.Now.AddHours(-24);
+                    break;
+                case "48h":
+                    fromDate = DateTime.Now.AddHours(-48);
+                    break;
+                case "1w":
+                    fromDate = DateTime.Now.AddDays(-7); // 1 semana
+                    break;
+                case "1m":
+                    fromDate = DateTime.Now.AddMonths(-1); // 1 mês
+                    break;
+                default:
+                    return BadRequest("Período inválido. Use 24h, 48h, 1w, ou 1m.");
+            }
+
+            // Buscar os dados dos sensores a partir do intervalo de tempo
+            var sensorData = _sensorContext.SensorData
+                                     .Where(s => s.TimeStamp >= fromDate)
+                                     .ToList();
+
+            if (sensorData.Count == 0)
+            {
+                return NotFound("Nenhum dado encontrado para o período especificado.");
+            }
+
+            // Calcular a média dos valores dos sensores
+            var averageValue = sensorData.Average(s => s.Value);
+
+            return Ok(new
+            {
+                period = period,
+                average = averageValue
+            });
         }
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,16 +1,32 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
 using OilGas.Data;
-using AutoMapper;
 using OilGas.Services;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Sensor Data API",
+        Description = "Essa API disponibiliza dados e operações sobre eles de uma planta de Óleo e Gás",
+    });
+
+    // Habilita a documentação via Swashbuckle
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
+
+
 
 //Dependency Injection
 builder.Services.AddScoped<SensorDataService>();
@@ -24,10 +40,6 @@ builder.Services.AddDbContext<SensorContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
-
-
-
-
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
